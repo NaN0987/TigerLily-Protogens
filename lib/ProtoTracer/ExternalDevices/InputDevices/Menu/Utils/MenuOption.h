b2922aeea3e35d1a798d34115886f19d5b723aa6
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2026-06-14 16:36:39 -0400

    Stubbed menu option struct
    My plan is to eventually change the code to use a list of menu option structs so that menu options can be added and modified more easily. This may be slightly less efficient in the end, but it will be significantly easier to work with than how the menu is currently implemented.

---
+++ lib/ProtoTracer/ExternalDevices/InputDevices/Menu/Utils/MenuOption.h
@@ -0,0 +1,19 @@
+/**
+ * @file MenuOption.h
+ * @brief Provides a struct for organizing menu options.
+ *
+ * This file defines a simple struct, MenuOption, which contains all of the data
+ * associated with a single menu option.
+ *
+ * @date 12/10/2025
+ * @author NaN0987
+ */
+
+#pragma once
+
+struct MenuOption {
+    uint8_t currentValue;         ///< The current value of the option.
+    const uint8_t maxValue;       ///< The maximum value the option can take.
+    const String optionName;  ///< The name of the option.
+    const bool isOnOff;       ///< If true, the menu will have the options "ON" and "OFF".
+};
